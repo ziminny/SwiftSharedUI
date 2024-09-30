8cac3ce097d7d475320e173714205caeadf58265
Vagner Reis <ziminny@gmail.com>
2024-09-30 14:56:29 -0700

    initial commit

---
+++ SharedUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SharedUI
+//
+//  Created by vagner reis on 26/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharedUI/SharedUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedUIApp.swift
+//  SharedUI
+//
+//  Created by vagner reis on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SharedUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
